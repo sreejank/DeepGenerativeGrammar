dc3b75d0d6be4c0a85c6c7f47996a949ca538647
Simon Segert <simonsegert@gmail.com>
2020-05-08 23:27:57 -0500

    experiment on 2-rule grammars

--- GeneratePolygons.py
+++ GeneratePolygons.py
@@ -26,12 +26,58 @@ def generate_batch(batch_size,n_sides,edge_length_range=(40,60),edge_width=2,cen
         imgs.append(image)
     return np.stack(imgs)
 
+
+
+def generate_batch_2rels(batch_size,s1,s2,edge_length_range=(20,30),edge_width=2,center_mean=(64,64),center_std=5,img_size=128):
+    #batch_size: number of images to generate
+    #s1,s2 number of sides for each of the two edge types (e.g. if s1=3, then one of the edge types
+    #will have 60 degree internal angle)
+    imgs=[]
+    for _ in range(batch_size):
+
+        image=np.zeros((img_size,img_size))
+
+        center=center_mean+center_std*np.random.randn(2)
+        edge_length=np.random.uniform(low=edge_length_range[0],high=edge_length_range[1])
+        theta0=np.random.rand()*2*np.pi
+
+        tot_n_vertices=np.random.choice(np.arange(3,max(5,max(s1,s2)))) #number of vertices not counting original one
+        #make sure at least one of each is chosen, to disambiguate from the one-relation grammars
+        side_types=[s1]*2
+        while(len(np.unique(side_types[1:]))<2):
+            side_types=np.random.choice([s1,s2],tot_n_vertices)
+
+        vertices=[center+np.dot(R(theta0),edge_length*np.array([1,0]))]
+        p0=vertices[0]
+
+        tot_angle=0
+        for i in range(tot_n_vertices):
+            v0=vertices[-1]
+            st=side_types[i]
+
+            angle=2*np.pi/st #external angle
+            tot_angle=tot_angle+angle
+            v_new=np.dot(R(tot_angle),edge_length*np.array([1,0]))+v0
+            vertices.append(v_new)
+
+
+
+
+        vertices=np.vstack(vertices)
+        for v,vnext in zip(vertices[:-1],vertices[1:]):
+            image=cv2.line(image, tuple(v.astype('int')), tuple(vnext.astype('int')), (255), edge_width)
+        imgs.append(image)
+    return np.stack(imgs)
+
+
+
+
 class PolygonDataset(Dataset):
-    def __init__(self,n_imgs_per_class,sides_range=[3,4,5,6,7,8,9,10]):
+    def __init__(self,n_imgs_per_class,sides_range=[3,4,5,6,7,8,9,10],normalize=False):
         self.imgs=[]
         self.labels=[]
         for class_id,n_sides in enumerate(sides_range):
-            batch=generate_batch(n_imgs_per_class,n_sides,edge_width=1,center_mean=(16,16),img_size=32,center_std=1,edge_length_range=(12,14)).astype('float32')
+            batch=generate_batch(n_imgs_per_class,n_sides,edge_width=8,center_mean=(16,16),img_size=32,center_std=2,edge_length_range=(12,14)).astype('float32')
             batch=np.expand_dims(batch,1)
             self.imgs.append(batch)
             labels=[class_id for _ in range(n_imgs_per_class)]
@@ -40,23 +86,13 @@ class PolygonDataset(Dataset):
         self.labels=np.hstack(self.labels)
         self.n=self.labels.shape[0]
         self.sides_range=sides_range
+        self.normalize=normalize
+        if(normalize):
+            self.imgs=.9*(self.imgs-.5)/.5
+
     def __len__(self):
         return self.n
     def __getitem__(self,idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        return (self.imgs[idx],self.labels[idx]) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return (self.imgs[idx],self.labels[idx])
